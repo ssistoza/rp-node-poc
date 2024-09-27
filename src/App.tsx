388047e1ab1b3c3c4badff629b2d684b2a6a9764
Shane Avery Sistoza <me@ssistoza.dev>
2024-09-27 08:55:51 -0400

    fix: cleanup (#20)

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more. (1)
+        Click on the Vite and React logos to learn more. (2)
       </p>
     </>
   );
